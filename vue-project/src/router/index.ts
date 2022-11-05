e8836c2ac0022d34e3d57b4f04fbdaf45f05e4fc
Tomoya Kobayashi <tomocha.marika@gmail.com>
2022-11-05 22:38:58 +0900

    add css

--- vue-project/src/router/index.ts
+++ vue-project/src/router/index.ts
@@ -1,11 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/contract',
+      path: '/',
       name: 'contract',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
