17be89d5e56ba9a3b88dcb01ad76044f6b82b41e
Tomoya Kobayashi <tomocha.marika@gmail.com>
2022-11-05 15:39:01 +0900

    succeeded to get name

---
+++ vue-project/src/views/FacebookView.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const FACEBOOK_API_URL = "https://graph.facebook.com/";
+const ACCESS_TOKEN = import.meta.env.VITE_ACCESS_TOKEN;
+const url = FACEBOOK_API_URL + "me?access_token=" + ACCESS_TOKEN;
+const load = async () => {
+  const res = await fetch(url).then(response => response.json()).catch(err => {
+    console.error(err);
+  });
+  if (res) {
+    console.log(res);
+  }
+}
+
+load();
+
+</script>
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h2>aa</h2>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
