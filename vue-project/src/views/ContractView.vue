238ef7ce26fc3d92579001db54ee0a6d181b738f
Tomoya Kobayashi <tomocha.marika@gmail.com>
2022-11-05 19:35:33 +0900

    refactoring

--- vue-project/src/views/ContractView.vue
+++ vue-project/src/views/ContractView.vue
@@ -1,33 +1,37 @@
 <script setup lang="ts">
 import { ethers } from "ethers";
+import type { Web3Provider } from "ethers-providers";
 import abi from "@/abi/contract_abi.json";
 import type { Contract_abi } from "@/types/ethers-contracts/index";
 
-if ((window as any).ethereum) {
-  const ethereum = (window as any).ethereum;
-  const acconts = await ethereum.request({ method: "eth_requestAccounts" }).catch(err => {
-    console.error(err);
-  });
-  console.log(acconts);
+const connect = async () => {
+  if ((window as any).ethereum) {
+    const ethereum = (window as any).ethereum;
+    const acconts = await ethereum.request({ method: "eth_requestAccounts" }).catch(err => {
+      console.error(err);
+    });
+    console.log(acconts);
 
-  const contractAddress = "0xbA3E9C379eAB31f1F98dD9Bf0d78faFe5b2Fe890";
-  const provider = new ethers.providers.Web3Provider(ethereum);
-  const signer = await provider.getSigner();
-  const Contract = await new ethers.Contract(contractAddress, abi, signer) as Contract_abi;
-  const recepientAddress = "0x5a10d152072832823c38e964E382CD22C00a8d7E";
-  const tokenUri = "https://gateway.pinata.cloud/ipfs/QmTFCG9UPu5gfa2edbXEZVcr6BLu8NLzV14DWCLsedNFUd";
+    const contractAddress = "0xbA3E9C379eAB31f1F98dD9Bf0d78faFe5b2Fe890";
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = await provider.getSigner();
+    const Contract = await new ethers.Contract(contractAddress, abi, signer) as Contract_abi;
+    const recepientAddress = "0x5a10d152072832823c38e964E382CD22C00a8d7E";
+    const tokenUri = "https://gateway.pinata.cloud/ipfs/QmTFCG9UPu5gfa2edbXEZVcr6BLu8NLzV14DWCLsedNFUd";
 
-  const receipt = await Contract.mintNFT(recepientAddress, tokenUri);
-  console.log(receipt);
+    const receipt = await Contract.mintNFT(recepientAddress, tokenUri);
+    console.log(receipt);
 
-  // const name = await Contract.name().catch(err => {
-  //   console.error(err);
-  // });
+    // const name = await Contract.name().catch(err => {
+    //   console.error(err);
+    // });
+  }
 }
 </script>
 
 
 <template>
+  <button @click="connect">connect</button>
   <div class="about">
     <h1>This is an about page</h1>
     <h2>aa</h2>
