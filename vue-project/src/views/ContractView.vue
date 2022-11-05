33529ad47972dfd7d450a4617afdba250d76822c
Tomoya Kobayashi <tomocha.marika@gmail.com>
2022-11-05 12:26:23 +0900

    succeeded to get address

---
+++ vue-project/src/views/ContractView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+if ((window as any).ethereum) {
+  const ethereum = (window as any).ethereum;
+  const acconts = await ethereum.request({ method: "eth_requestAccounts" }).catch(err => {
+    console.error(err);
+  });
+  console.log(acconts);
+}
+</script>
+
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h2>aa</h2>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
